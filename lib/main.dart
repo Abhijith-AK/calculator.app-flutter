ae0f9e27af86ac84cb4f088b58f3fff41858c6aa
Abhijith Krishna A K <138287385+Abhijith-AK@users.noreply.github.com>
2024-05-21 23:28:04 +0530

    release commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(
@@ -26,19 +27,80 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  var equation = "";
+  var result = "";
+  var expression = "";
+
+  calculation(String buttonText) {
+    // used to check if the result contains a decimal
+    String doesContainDecimal(dynamic result) {
+      if (result.toString().contains('.')) {
+        List<String> splitDecimal = result.toString().split('.');
+        if (!(int.parse(splitDecimal[1]) > 0)) {
+          return result = splitDecimal[0].toString();
+        }
+      }
+      return result;
+    }
+
+    setState(() {
+      if (buttonText == "AC") {
+        equation = "0";
+        result = "0";
+      } else if (buttonText == "⌫") {
+        equation = equation.substring(0, equation.length - 1);
+        if (equation == "") {
+          equation = "0";
+        }
+      } else if (buttonText == "+/-") {
+        if (equation[0] != '-') {
+          equation = '-$equation';
+        } else {
+          equation = equation.substring(1);
+        }
+      } else if (buttonText == "=") {
+        expression = equation;
+        expression = expression.replaceAll('X', '*');
+        expression = expression.replaceAll('÷', '/');
+        expression = expression.replaceAll('%', '%');
+
+        try {
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+
+          ContextModel cm = ContextModel();
+          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+          if (expression.contains('%')) {
+            result = doesContainDecimal(result);
+          }
+        } catch (e) {
+          result = "Error";
+        }
+      } else {
+        if (equation == "0") {
+          equation = buttonText;
+        } else {
+          equation = equation + buttonText;
+        }
+      }
+    });
+  }
+
   Widget calcButton(String btntxt, Color btncolor, Color textcolor) {
     return ElevatedButton(
       onPressed: () {
-        //TODO function to add argument of button
+        calculation(btntxt);
       },
       style: ButtonStyle(
         backgroundColor: MaterialStatePropertyAll(btncolor),
         shape: const MaterialStatePropertyAll(CircleBorder()),
-        padding: const MaterialStatePropertyAll(EdgeInsets.all(20)),
+                padding: const MaterialStatePropertyAll(EdgeInsets.all(25)),
+
       ),
       child: Text(
         btntxt,
-        style: TextStyle(color: textcolor, fontSize: 35),
+        textAlign: TextAlign.center,
+        style: TextStyle(color: textcolor, fontSize: 25),
       ),
     );
   }
@@ -56,82 +118,104 @@ class _CalculatorState extends State<Calculator> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            //Calculator Display
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    '0',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 100,
+        child: Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              //Calculator Display
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          equation,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(color: Colors.grey, fontSize: 45),
+                        ),
+                        Text(
+                          result,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 75,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    //Where calculate buttons are passed
+                    calcButton('AC', Colors.grey, Colors.black),
+                    calcButton('⌫', Colors.grey, Colors.black),
+                    calcButton('%', Colors.amber.shade700, Colors.white),
+                    calcButton('/', Colors.amber.shade700, Colors.white),
+                  ],
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //Where calculate buttons are passed
-                calcButton('AC', Colors.grey, Colors.black),
-                calcButton('<-', Colors.grey, Colors.black),
-                calcButton('%', Colors.amber.shade700, Colors.white),
-                calcButton('/', Colors.amber.shade700, Colors.white),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //Where calculate buttons are passed
-                calcButton('7', Colors.grey.shade800, Colors.white),
-                calcButton('8', Colors.grey.shade800, Colors.white),
-                calcButton('9', Colors.grey.shade800, Colors.white),
-                calcButton('*', Colors.amber.shade700, Colors.white),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //Where calculate buttons are passed
-                calcButton('4', Colors.grey.shade800, Colors.white),
-                calcButton('5', Colors.grey.shade800, Colors.white),
-                calcButton('6', Colors.grey.shade800, Colors.white),
-                calcButton('-', Colors.amber.shade700, Colors.white),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //Where calculate buttons are passed
-                calcButton('1', Colors.grey.shade800, Colors.white),
-                calcButton('2', Colors.grey.shade800, Colors.white),
-                calcButton('3', Colors.grey.shade800, Colors.white),
-                calcButton('+', Colors.amber.shade700, Colors.white),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //Where calculate buttons are passed
-                calcButton('+/-', Colors.grey.shade800, Colors.white),
-                calcButton('0', Colors.grey.shade800, Colors.white),
-                calcButton('.', Colors.grey.shade800, Colors.white),
-                calcButton('=', Colors.amber.shade700, Colors.white),
-              ],
-            ),
-            const SizedBox(height: 10),
-          ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    //Where calculate buttons are passed
+                    calcButton('7', Colors.grey.shade800, Colors.white),
+                    calcButton('8', Colors.grey.shade800, Colors.white),
+                    calcButton('9', Colors.grey.shade800, Colors.white),
+                    calcButton('X', Colors.amber.shade700, Colors.white),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    //Where calculate buttons are passed
+                    calcButton('4', Colors.grey.shade800, Colors.white),
+                    calcButton('5', Colors.grey.shade800, Colors.white),
+                    calcButton('6', Colors.grey.shade800, Colors.white),
+                    calcButton('-', Colors.amber.shade700, Colors.white),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    //Where calculate buttons are passed
+                    calcButton('1', Colors.grey.shade800, Colors.white),
+                    calcButton('2', Colors.grey.shade800, Colors.white),
+                    calcButton('3', Colors.grey.shade800, Colors.white),
+                    calcButton('+', Colors.amber.shade700, Colors.white),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    //Where calculate buttons are passed
+                    calcButton('+/-', Colors.grey.shade800, Colors.white),
+                    calcButton('0', Colors.grey.shade800, Colors.white),
+                    calcButton('.', Colors.grey.shade800, Colors.white),
+                    calcButton('=', Colors.amber.shade700, Colors.white),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
